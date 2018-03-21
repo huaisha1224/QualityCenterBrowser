b789421ef3298b3e4840cdbc48eb79737914d8d7
Sam.Huang <sam.hxq@gmail.com>
2018-03-21 10:03:26 +0800

    Update QCBrowser.py
    用Python3.6 重新编写

--- QCBrowser.py
+++ QCBrowser.py
@@ -1,57 +1,66 @@
 #!/usr/bin/python
 #!encoding:utf-8
 #!filename:QCBrowser.py
-#----------------------------------------------------------------------------
+#Python3.6
+#-------------------------------------------------------------------
 # Name:         QCBrowser.py
 # Purpose:      用于访问HP Quality Center服务器的浏览器客户端
 # Author:       Sam Huang
 # Email:        sam.hxq@gmail.com
 # Blog:         www.hiadmin.org
-# Copyright:    (c) 2014 by Sam.huang
+# Copyright:    (c) 2018 by Sam.huang
 # Licence:      wxWindows license
-#----------------------------------------------------------------------
+
+#---------------------------------------------------------------------
+name = "Quality Center Browser"
+version = " 1.5"
+Build = "20180321"
+#---------------------------------------------------------------------
 import wx
 import wx.html2 as webview
 import sys, os
-import ConfigParser
-import webbrowser
 import requests
-#----------------------------------------------------------------------
-name = "Quality Center Browser"
-version = " 1.2"
-Build = "201410710"
+import webbrowser
+import configparser
+
 
 #----------------------------------------------------------------------
+
 class QualityCenterBrowser(wx.Panel):
     """创建主函数
     创建button、StaticText并绑定函数事件
-
     """
-    def __init__(self, parent,HomePage, frame=None):
+    def __init__(self, parent, log, frame=None):
+        #self.log = log
         wx.Panel.__init__(self, parent, -1)
+
+        #self.HomePage = "http://qc.hiadmin.org"
         self.HomePage = HomePage
-        self.frame = frame      
+        self.frame = frame
+        if frame:
+            self.titleBase = frame.GetTitle()
+
         sizer = wx.BoxSizer(wx.VERTICAL)
         btnSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.wv = webview.WebView.New(self)
+
         #显示地址栏URL地址
         self.Bind(webview.EVT_WEBVIEW_LOADED, self.OnWebViewLoaded, self.wv)
-        
-        #创建回退按钮
+
+
+        #创建返回按钮
         btn = wx.BitmapButton(self, -1, wx.Bitmap("src/Left.png"))
         self.Bind(wx.EVT_BUTTON, self.OnPrevPageButton, btn)
         btnSizer.Add(btn, 0, wx.EXPAND|wx.ALL, 2)
-        self.Bind(wx.EVT_UPDATE_UI, self.OnCheckCanGoBack, btn)
 
         #创建前进按钮
         btn = wx.BitmapButton(self, -1, wx.Bitmap("src/Right.png"))
         self.Bind(wx.EVT_BUTTON, self.OnNextPageButton, btn)
         btnSizer.Add(btn, 0, wx.EXPAND|wx.ALL, 2)
-        self.Bind(wx.EVT_UPDATE_UI, self.OnCheckCanGoForward, btn)
 
         #创建刷新按钮
-        btn = wx.BitmapButton(self, -1,wx.Bitmap("src/Reload.png"))
-        self.Bind(wx.EVT_BUTTON, self.OnRefreshPageButton, btn)
+        btn = wx.BitmapButton(self, -1, wx.Bitmap("src/Reload.png"))
+        self.Bind(wx.EVT_BUTTON, self.OnStopButton, btn)
         btnSizer.Add(btn, 0, wx.EXPAND|wx.ALL, 2)
 
         #创建一个主页按钮
@@ -59,15 +68,12 @@ class QualityCenterBrowser(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.OnHomePage, btn)
         btnSizer.Add(btn, 0, wx.EXPAND|wx.ALL, 2)
 
-        #创建txt描述
         txt = wx.StaticText(self, -1, "Address:")
         btnSizer.Add(txt, 0, wx.CENTER|wx.ALL, 2)
 
-        #创建地址栏下来菜单中的URL地址列表
-        self.location = wx.ComboBox(self, -1, "", 
-                            style=wx.CB_DROPDOWN|wx.TE_PROCESS_ENTER)
-        self.location.AppendItems(['http://qc.hiadmin.org/',
-                                    'http://www.whatbrowser.org/intl/zh-CN/'])
+        self.location = wx.ComboBox(
+            self, -1, "", style=wx.CB_DROPDOWN|wx.TE_PROCESS_ENTER)
+        self.location.AppendItems('http://qc.hiadmin.org')
         self.Bind(wx.EVT_COMBOBOX, self.OnLocationSelect, self.location)
         self.location.Bind(wx.EVT_TEXT_ENTER, self.OnLocationEnter)
         btnSizer.Add(self.location, 1, wx.EXPAND|wx.ALL, 2)
@@ -80,23 +86,27 @@ class QualityCenterBrowser(wx.Panel):
 
         #创建升级按钮
         btn = wx.BitmapButton(self, -1,wx.Bitmap("src/Download.png"))
-        #btn = wx.Button(self, -1, "Update", style=wx.BU_EXACTFIT)
         self.Bind(wx.EVT_BUTTON, self.OnUpdate, btn)
         btnSizer.Add(btn, 0, wx.EXPAND|wx.ALL, 2)
-
 
         sizer.Add(btnSizer, 0, wx.EXPAND)
         sizer.Add(self.wv, 1, wx.EXPAND)
         self.SetSizer(sizer)
+
         self.wv.LoadURL(self.HomePage)
 
 
-    # WebView events
+    def ShutdownDemo(self):
+        # put the frame title back
+        if self.frame:
+            self.frame.SetTitle(self.titleBase)
+
     def OnWebViewLoaded(self, evt):
-        # 地址栏显示完整的URL地址
+        # The full document has loaded
         self.HomePage = evt.GetURL()
         self.location.SetValue(self.HomePage)
-        
+
+
     # Control bar events
     def OnLocationSelect(self, evt):
         url = self.location.GetStringSelection()
@@ -115,12 +125,17 @@ class QualityCenterBrowser(wx.Panel):
 
     def OnCheckCanGoBack(self, event):
         event.Enable(self.wv.CanGoBack())
-        
+
     def OnCheckCanGoForward(self, event):
         event.Enable(self.wv.CanGoForward())
 
+    def OnStopButton(self, evt):
+        self.wv.Stop()
+
     def OnRefreshPageButton(self, evt):
         self.wv.Reload()
+
+
     #按钮主页事件
     def OnHomePage(self, event):
         url = HomePage
@@ -131,19 +146,21 @@ class QualityCenterBrowser(wx.Panel):
 
     def OnUpdate(self,evt):
         webbrowser.open_new_tab("http://qc.hiadmin.org/download/")
-              
+#----------------------------------------------------------------------
+
+
 #----------------------------------------------------------------------
 def QCBrowserRun(frame, nb):
     """
     如果配置文件存在就从配置文件里面读取配置信息、否则就默认配置
     """
-    if os.path.exists("config.ini") == True: 
-        cf = ConfigParser.ConfigParser()
+    if os.path.exists("config.ini") == True:
+        cf = configparser.ConfigParser()
         cf.read("config.ini")
         global HomePage
         HomePage = cf.get("info","url") #默认主页
-        #if "192.168" not in HomePage:
-            #HomePage = "http://qc.hiadmin.org/direction/"
+        if "192.168" not in HomePage:
+            HomePage = "http://qc.hiadmin.org/direction/"
     else:
         HomePage = "http://qc.hiadmin.org/direction/"
 
@@ -154,25 +171,26 @@ def QCBrowserRun(frame, nb):
 class QualityCenterBrowserApp(wx.App):
     """创建窗口控件
     """
+
     def __init__(self, module):
         self.name = name
         self.demoModule = module
         wx.App.__init__(self)
 
     def OnInit(self):
-        #设置浏览器窗口大小，名称等信息
-        frame = wx.Frame(None, -1, self.name + version, size=(950,650))
-        frame.SetIcon(wx.Icon("src\QCBrowser_32.ico",wx.BITMAP_TYPE_ICO))
-        frame.CreateStatusBar() #创建状态栏
+        # 设置浏览器窗口大小，名称等信息
+        frame = wx.Frame(None, -1, self.name + version, size=(1027, 768))
+        frame.SetIcon(wx.Icon("src\QCBrowser_32.ico", wx.BITMAP_TYPE_ICO))
+        frame.CreateStatusBar()  # 创建状态栏
 
         ns = {}
         ns['wx'] = wx
         ns['app'] = self
         ns['module'] = self.demoModule
         ns['frame'] = frame
-        
+
         frame.Show(True)
-        win = self.demoModule.QCBrowserRun(frame, frame)     
+        win = self.demoModule.QCBrowserRun(frame, frame)
         return True
 
 #----------------------------------------------------------------------------
@@ -183,7 +201,4 @@ def QCBrowserApp(argv):
 
 #----------------------------------------------------------------------
 if __name__ == '__main__':
-    tongji = "http://qc.hiadmin.org/tongji.html"
-    t = requests.get(tongji)
     QCBrowserApp(['QCBrowser.py'])
-
